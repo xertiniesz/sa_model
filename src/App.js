b9a57bfdb580f3997c628790e65cb29e2aae8fca
Teerapat Parktoop <t.parktoop@gmail.com>
2019-02-02 03:35:13 +0700

    add engrave, need finishing touch

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import React, { Component } from 'react';
 import { Container, Grid } from 'semantic-ui-react';
 import Menu from './menu';
 import Model from './model';
+import TopBar from './topBar';
+import Cover from './cover/cover';
+import NavBar from './navBar/navBar';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <Grid columns={2}>
-        <Grid.Column width={4}>
-          <Menu />
-        </Grid.Column>
-        <Grid.Column width={12}>
-          <Model />
-        </Grid.Column>
-      </Grid>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Menu/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/menu.js
+++ src/menu.js
@@ -1,51 +1,102 @@
 import React, { Component } from 'react';
-import { Dropdown, Button, Menu } from 'semantic-ui-react'
+import { Dropdown, Button, Menu, TextArea, Form, Grid } from 'semantic-ui-react'
 import './semantic/dist/semantic.min.css';
-import './menu.css'
+import './menu.css';
+import Model from './model';
 
-const rings=[ {key: 'r1', value: 'r1', text: 'Ring1'},
-                  {key: 'r2', value: 'r2', text: 'Ring2'},
-                  {key: 'r3', value: 'r3', text: 'Ring3'}]
-const items = [
-  { key: 'editorials', active: true, name: 'Editorials' },
-  { key: 'review', name: 'Reviews' },
-  { key: 'events', name: 'Upcoming Events' },
+const rings=[ {key: 'r1', value: 'Ring1', text: 'Ring1'},
+                {key: 'r2', value: 'Ring2', text: 'Ring2'},
+                {key: 'r3', value: 'Ring3', text: 'Ring3'}]
+const size = [
+    { key: 's48', value: '48', text: '48' },
+    { key: 's49', value: '49', text: '49' },
+    { key: 's50', value: '50', text: '50' },
+    { key: 's51', value: '51', text: '51' },
+    { key: 's52', value: '52', text: '52' },
+    { key: 's53', value: '53', text: '53' },
+    { key: 's54', value: '54', text: '54' },
+    { key: 's55', value: '55', text: '55' },
+    { key: 's56', value: '56', text: '56' },
+    { key: 's57', value: '57', text: '57' },
 ]
+const fonts = [
+    { key: 'f1', value: 'San Serif', text: 'Sand Serif'},
+    { key: 'f2', value: 'San Serif', text: 'Sand Serif'},
+    { key: 'f3', value: 'San Serif', text: 'Sand Serif'},
+    { key: 'f4', value: 'San Serif', text: 'Sand Serif'},
+]
+
 class _Menu extends Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      selectedRing: "Ring1"
+    constructor (props) {
+        super(props);
+        this.state = {
+            selectedColor: '#C0C0C0',
+            selectedRing: "Ring1",
+            selectedSize: "48",
+            selectedFont: "San Serif",
+            engraving: "",
+        }
     }
-  }
 
-  handleChange = (event) => {
-    this.setState({selectedRing: event.target.value});
-  }
+    handleModelChange = (e, data) => {
+        this.setState({selectedRing: data.value});
+    }
+    handleSizeChange = (e, data) => {
+        this.setState({selectedSize: data.value});
+    }
+    handleFontChange = (e, data) => {
+        this.setState({selectedFont: data.value});
+    }
+    handleTextChange = (event) => {
+        console.log(event.target.value);
+        // this.setState({engraving: event.target.value});
+    }
 
-  render () {
-    return (
-      <Menu vertical size={'massive'} >
-        <Menu.Item>
-          <Menu.Header>แบบแหวน</Menu.Header>
-          <Dropdown text={this.state.selectedRing} selection options={rings} onChange={this.handleChange}/>
-        </Menu.Item>
-        <Menu.Item>
-          <Menu.Header>สีตัวเรือน</Menu.Header>
-          <Button color='#000000'/>
-          <Button className="ring color2"/>
-          <Button className="ring color3"/>
-          <Button className="ring color4"/>
-        </Menu.Item>
-        <Menu.Item>
-          <Menu.Header>ขนาด</Menu.Header>
-        </Menu.Item>
-        <Menu.Item>
-          <Menu.Header>สลัก</Menu.Header>
-        </Menu.Item>
-      </Menu>
-    );
-  }
+    render () {
+        return (
+            <div>
+                {/* <Grid centered>
+                    <Grid.Column width={12}> */}
+                        <Grid columns={2}>
+                            <Grid.Column width={4}>
+                                <Menu vertical size={'massive'} >
+                                    <Menu.Item>
+                                    <Menu.Header>แบบแหวน</Menu.Header>
+                                        <Dropdown text={this.state.selectedRing} selection options={rings} onChange={this.handleModelChange}/>
+                                    </Menu.Item>
+                                    <Menu.Item>
+                                    <Menu.Header>สีตัวเรือน</Menu.Header>
+                                        <Button className="ring color1" onClick={()=>{this.setState({selectedColor: '#C0C0C0'})}}/>
+                                        <Button className="ring color2" onClick={()=>{this.setState({selectedColor: '#FFC30D'})}}/>
+                                        <Button className="ring color3" onClick={()=>{this.setState({selectedColor: '#FFD2B4'})}}/>
+                                        <Button className="ring color4" onClick={()=>{this.setState({selectedColor: '#402F27'})}}/>
+                                    </Menu.Item>
+                                    <Menu.Item>
+                                        <Menu.Header>ขนาด</Menu.Header>
+                                        <Dropdown text={this.state.selectedSize} selection options={size} onChange={this.handleSizeChange}/>
+                                    </Menu.Item>
+                                    <Menu.Item>
+                                        <Menu.Header>สลัก</Menu.Header>
+                                        <div>ลักษณะตัวหนังสือ</div>
+                                        <Dropdown text={this.state.selectedFont} selection options={fonts} onChange={this.handleFontChange}/>
+                                        <div>ข้อความ</div>
+                                        <Form>
+                                            <TextArea autoHeight placeholder='ใส่ข้อความบนแหวน'/>
+                                        </Form>
+                                    </Menu.Item>
+                                </Menu>
+                                <Button color="green" onClick={()=>{console.log(this.state)}}>OKAY</Button>
+                                {/* <Button color="red">RESET</Button> */}
+                            </Grid.Column>
+                            <Grid.Column width={12}>
+                                <Model ring={this.state.selectedRing} color={this.state.selectedColor}/>
+                            </Grid.Column>
+                        </Grid>
+                    {/* </Grid.Column>
+                </Grid> */}
+            </div>
+        );
+    }
 }
 
 export default _Menu
